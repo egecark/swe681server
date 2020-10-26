2d789f4bcc75e92d5454707956c5c4e9bdbc818b
egecark <ecarkoglu14@ku.edu.tr>
2020-10-26 15:49:26 +0300

    created dummy api

--- server/testing/views.py
+++ server/testing/views.py
@@ -1,22 +1,11 @@
-from django.contrib.auth.models import User, Group
+from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework import permissions
-from server.testing.serializers import UserSerializer, GroupSerializer
+
+from .serializers import DummySerializer
+from .models import Dummy
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+class DummyViewSet(viewsets.ModelViewSet):
+    queryset = Dummy.objects.all().order_by('value')
+    serializer_class = DummySerializer
+# Create your views here.
