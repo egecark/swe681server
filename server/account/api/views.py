16260b534e32a6c4f6531cacf9142e525fc8f977
anthonym346 <anthony.j.matthews24@gmail.com>
2020-12-03 03:52:35 +0000

    Updates for registration failed registration no longer sends the error message to the user. restart_server script fixed. client updated with registration page

--- server/account/api/views.py
+++ server/account/api/views.py
@@ -10,8 +10,13 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.decorators import api_view, permission_classes
 
-@api_view(['POST', ])
+#need to regex the username, email, and password
+#probably should not return the error as it could give attacker info
+@api_view(['GET', 'POST', ])
+@permission_classes([])
 def registration_view(request):
+	if request.method == 'GET':
+		return render(request, 'register/index.html')
 
 	if request.method == 'POST':
 		serializer = RegistrationSerializer(data=request.data)
@@ -24,9 +29,11 @@ def registration_view(request):
 			token = Token.objects.get(user=account).key
 			data['token'] = token
 		else:
-			data = serializer.errors
+			#data = serializer.errors
+			data['response'] = 'invalid login'
 		return Response(data)
 
+#need to regex the username and password
 @api_view(['GET', 'POST'])
 @permission_classes([])
 def login_view(request):
--- server/restart_server.sh
+++ server/restart_server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo systemctl restart apache2
 sleep 5
-chmod 664 ~/myproject/swe681server/server/db.sqlite3
+sudo chmod 664 ~/myproject/swe681server/server/db.sqlite3
 sudo chown :www-data ~/myproject/swe681server/server/db.sqlite3
 sudo chown :www-data ~/myproject/swe681server
+sudo chown :www-data ~/myproject/swe681server/server
 
